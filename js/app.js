c17bdf2a0f29668afe2437ac454a58cd7e3dd522
brunoaferreira90 <bruno.a.ferreira09@hotmail.com>
2019-04-03 17:42:05 -0300

    correção path

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 
         .controller('ExtratoController', function ($scope, $http) {
 			
-			$http.get('http://localhost:8080/api/extrato/getExtrato/').then(function(response) {
+			$http.get('http://localhost:8080/api/extrato/').then(function(response) {
 				$scope.extratos = response.data;
 			});
 
